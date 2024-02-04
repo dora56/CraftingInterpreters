7215098a71cfc48ba7719c1c3e5fc73ca74a7790
dora56 <doradora56@gmail.com>
2024-02-04 22:52:35 +0900

    5.2.2 まで実装

---
+++ Tool/GenerateAst.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+
+namespace Tool;
+
+public class GenerateAst
+{
+    public static void Main(string[] args)
+    {
+        if(args.Length != 1)
+        {
+            Console.Error.WriteLine("Usage: generate_ast <output directory>");
+            Environment.Exit(64);
+        }
+        var outputDir = args[0];
+        DefineAst(
+            outputDir,
+            "Expr",
+            [
+                "Binary   : Expr left, Token oprator, Expr right",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator, Expr right"
+            ]);
+    }
+
+    public static void DefineAst(
+        string outputDir,
+        string baseName,
+        IEnumerable<string> types)
+    {
+        var path = $"{outputDir}/{baseName}.cs";
+        using var writer = new StreamWriter(path, false);
+        writer.WriteLine("namespace Lox;");
+        writer.WriteLine();
+        writer.WriteLine($"public abstract class {baseName}");
+        writer.WriteLine("{");
+        types.ToList().ForEach(type =>
+        {
+            var className = type.Split(":")[0].Trim();
+            var fields = type.Split(":")[1].Trim();
+            DefineType(writer, baseName, className, fields);
+        });
+        writer.WriteLine("}");
+
+    }
+
+    private static void DefineType(
+        StreamWriter writer,
+        string baseName,
+        string className,
+        string fieldList)
+    {
+        writer.WriteLine("    public class " + className + " : " + baseName);
+        writer.WriteLine("    {");
+        writer.WriteLine("        public " + className + "(" + fieldList + ")");
+        
+        var fields = fieldList.Split(", ").ToList();
+        fields.ForEach(field =>
+        {
+            var name = field.Split(" ")[1];
+            writer.WriteLine("            " + field + ")");
+        });
+        
+        writer.WriteLine("        }");
+        
+        fields.ForEach(field =>
+        {
+            var name = field.Split(" ")[1];
+            writer.WriteLine("        public " + field + " { get; }");
+        });
+        
+        writer.WriteLine("    }");
+        
+        writer.WriteLine();
+        fields.ForEach(field =>
+            writer.WriteLine($"        public {field} {{ get; internal set; }}"));
+        writer.WriteLine("    }");
+    }
+}
