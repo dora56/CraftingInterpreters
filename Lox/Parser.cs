8b9f16c634912b16609e96a3e7e0c9e2947f9019
dora56 <doradora56@gmail.com>
2024-02-11 17:47:01 +0900

    6.2.1 まで実装

---
+++ Lox/Parser.cs
@@ -0,0 +1,141 @@
+namespace Lox;
+
+public class Parser
+{
+    private readonly List<Token> _tokens;
+    private int _current = 0;
+    
+    public Parser(List<Token> tokens)
+    {
+        _tokens = tokens;
+    }
+    
+    private Expr Expression()
+    {
+        return Equality();
+    }
+
+    private Expr Equality()
+    {
+        var expr = Comparison();
+
+        while (Match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL))
+        {
+            var @operator = Previous();
+            var right = Comparison();
+            expr = new Expr.Binary(expr, @operator, right);
+        }
+
+        return expr;
+    }
+
+    private bool Match(params TokenType[] types)
+    {
+        foreach (var type in types)
+        {
+            if (Check(type))
+            {
+                Advance();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private bool Check(TokenType type)
+    {
+        if (IsAtEnd()) return false;
+        return Peek().Type == type;
+    }
+
+    private Token Advance()
+    {
+        if(!IsAtEnd()) _current++;
+        return Previous();
+    }
+
+    private Token Previous()
+    {
+        return _tokens[_current - 1];
+    }
+
+    private bool IsAtEnd()
+    {
+        return Peek().Type == TokenType.EOF;
+    }
+    
+    private Token Peek()
+    {
+        return _tokens[_current];
+    }
+    
+    private Expr Comparison()
+    {
+        var expr = Term();
+
+        while (Match(TokenType.GREATER, TokenType.GREATER_EQUAL, TokenType.LESS, TokenType.LESS_EQUAL))
+        {
+            var @operator = Previous();
+            var right = Term();
+            expr = new Expr.Binary(expr, @operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr Term()
+    {
+        var expr = Factor();
+
+        while (Match(TokenType.MINUS, TokenType.PLUS))
+        {
+            var @operator = Previous();
+            var right = Factor();
+            expr = new Expr.Binary(expr, @operator, right);
+        }
+        return expr;
+    }
+
+    private Expr Factor()
+    {
+        var expr = Unary();
+        if (Match(TokenType.SLASH, TokenType.STAR))
+        {
+            var @operator = Previous();
+            var right = Unary();
+            return new Expr.Binary(expr, @operator, right);
+        }
+        return expr;
+    }
+
+    private Expr Unary()
+    {
+        if (Match(TokenType.BANG, TokenType.MINUS))
+        {
+            var @operator = Previous();
+            var right = Unary();
+            return new Expr.Unary(@operator, right);
+        }
+        return Primary();
+    }
+
+    private Expr Primary()
+    {
+        if(Match(TokenType.FALSE)) return new Expr.Literal(false);
+        if(Match(TokenType.TRUE)) return new Expr.Literal(true);
+        if (Match(TokenType.NIL)) return new Expr.Literal(null);
+
+        if (Match(TokenType.NUMBER, TokenType.STRING))
+        {
+            return new Expr.Literal(Previous().Literal);
+        }
+
+        if (Match(TokenType.LEFT_PAREN))
+        {
+            var expr = Expression();
+            Consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
+    }
+}
--- Lox/Token.cs
+++ Lox/Token.cs
@@ -2,9 +2,9 @@ namespace Lox;
 
 public class Token(TokenType type, string lexeme, object? literal, int line)
 {
-    private TokenType Type { get; init; } = type;
+    public TokenType Type { get; init; } = type;
     public string Lexeme { get; init; } = lexeme;
-    object? Literal { get; init; } = literal;
+    public object? Literal { get; init; } = literal;
     public int Line { get; init; } = line;
 
     public override string ToString()
